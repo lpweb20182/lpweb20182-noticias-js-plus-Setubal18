385f611a0e8e415356db4997d7b423a2b45e3d47
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-07-23 21:58:34 -0300

    issui 2

--- main.js
+++ main.js
@@ -11,34 +11,59 @@ function atualizarLista(noticia) {
         + '<p class="conteudo">'
         + noticia.conteudo
         + '<br>'
+        + '<small>'
+        + noticia.nome
+        + noticia.email
+        + '<br>'
+        + noticia.data
+        + '</small>'
+        + '<br>'
         + '<span>------------------</span>'
         + '<br>'
         + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
-        + '</p>';
+    +'</p>';
     lista.appendChild(li);
 }
 
 function salvar(form) {
     var titulo = document.getElementById('frm-titulo').value;
     var conteudo = document.getElementById('frm-conteudo').value;
+    var data = new Date(document.getElementById('frm-data').value);
+    var email = document.getElementById('frm-email').value;
+    var nome = document.getElementById('frm-nome').value;
+    var datahj = new Date()
     var noticia = {
         id: noticias.length,
         titulo: titulo,
-        conteudo: conteudo
+        conteudo: conteudo,
+        data: data,
+        nome: nome,
+        email:email
     };
-    noticias.push(noticia);
-    atualizarLista(noticia);
-    form.reset();
+
+    if (data <= datahj && data != '') {
+       noticias.push(noticia);
+       atualizarLista(noticia);
+       form.reset();
+
+    }
+
+    else {
+        alert("Ou você veio do futuro ou a data está errada !");
+    }
+
+
 }
 
 function mostrarNoticia(id) {
-    var li = document.getElementById('noticia-' + id);
-    for (var i = 0; i < li.childNodes.length; i++) {
-        var node = li.childNodes[i];
-        if (node.getAttribute('class') == 'conteudo') {
-            node.setAttribute('style', 'display:inline');
+        var li = document.getElementById('noticia-' + id);
+        for (var i = 0; i < li.childNodes.length; i++) {
+            var node = li.childNodes[i];
+            if (node.getAttribute('class') == 'conteudo') {
+                node.setAttribute('style', 'display:inline');
+            }
         }
-    }
+
 }
 
 function ocultarNoticia(id) {
