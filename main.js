1594377f681a599ec8e5d3c567499e1fb42df2f8
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-07-27 17:39:04 -0300

    .

--- main.js
+++ main.js
@@ -3,27 +3,27 @@ var noticias = [];
 function atualizarLista(noticia) {
     var lista = document.getElementById('noticias-recentes-list');
     var li = document.createElement('li');
-    li.setAttribute('id', 'noticia-' + noticia.id);
-    li.setAttribute('class', 'noticia');
-    li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
-        + noticia.titulo
-        + '</p>'
-        + '<p class="conteudo">'
-        + noticia.conteudo
-        + '<br>'
-        + '<small>'
-        + noticia.nome
-        + noticia.email
-        + '<br>'
-        + noticia.data
-        + '</small>'
-        + '<br>'
-        + '<span>------------------</span>'
-        + '<br>'
-        + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
-    +'</p>';
-    lista.appendChild(li);
-}
+        li.setAttribute('id', 'noticia-' + noticia.id);
+        li.setAttribute('class', 'noticia');
+        li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
+            + noticia.titulo
+            + '</p>'
+            + '<p class="conteudo">'
+            + noticia.conteudo
+            + '<br>'
+            + '<small>'
+            + noticia.nome
+            + noticia.email
+            + '<br>'
+            + noticia.data
+            + '</small>'
+            + '<br>'
+            + '<span>------------------</span>'
+            + '<br>'
+            + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
+        +'</p>';
+        lista.appendChild(li);
+    }
 
 function salvar(form) {
     var titulo = document.getElementById('frm-titulo').value;
@@ -41,25 +41,22 @@ function salvar(form) {
         email:email
     };
 
-    if (data <= datahj && data != '') {
+    if (data <= datahj) {
        noticias.push(noticia);
        atualizarLista(noticia);
        form.reset();
 
     }
-
-    else {
+      else {
         alert("Ou você veio do futuro ou a data está errada !");
     }
-
-
 }
 
 function mostrarNoticia(id) {
         var li = document.getElementById('noticia-' + id);
         for (var i = 0; i < li.childNodes.length; i++) {
             var node = li.childNodes[i];
-            if (node.getAttribute('class') == 'conteudo') {
+            if (node.getAttribute('class') == 'conteudo' ) {
                 node.setAttribute('style', 'display:inline');
             }
         }
